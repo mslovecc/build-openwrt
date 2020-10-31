8c0bed086ef478ca738f3e639741dd4bd5a65a07
mslovecc <33970221+mslovecc@users.noreply.github.com>
2020-10-31 19:55:44 +0800

    Update copy-amd64.sh

--- lede/copy-amd64.sh
+++ lede/copy-amd64.sh
@@ -4,6 +4,6 @@ cp $GITHUB_WORKSPACE/lede/shadow $GITHUB_WORKSPACE/openwrt/package/base-files/fi
 cp $GITHUB_WORKSPACE/lede/hosts $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/hosts
 cp $GITHUB_WORKSPACE/lede/config_generate $GITHUB_WORKSPACE/openwrt/package/base-files/files/bin/config_generate
 cp $GITHUB_WORKSPACE/lede/mac80211.sh $GITHUB_WORKSPACE/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-cp $GITHUB_WORKSPACE/lede/luci $GITHUB_WORKSPACE/openwrt/feeds/luci/modules/luci-base/root/etc/config/luci
+#cp $GITHUB_WORKSPACE/lede/luci $GITHUB_WORKSPACE/openwrt/feeds/luci/modules/luci-base/root/etc/config/luci
 cp $GITHUB_WORKSPACE/lede/dhcp.conf $GITHUB_WORKSPACE/openwrt/package/network/services/dnsmasq/files/dhcp.conf
 #cp $GITHUB_WORKSPACE/lede/dnsmasq.conf $GITHUB_WORKSPACE/openwrt/package/network/services/dnsmasq/files/dnsmasq.conf
